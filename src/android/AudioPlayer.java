6b23154a35147ce09053e66058d982fb9abe9800
Piotr.Hauza <p.hauza@cctechnology.pl>
2017-01-13 12:13:04 +0100

    ph01

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -630,7 +630,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
     private void loadAudioFile(String file) throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
         if (this.isStreaming(file)) {
             this.player.setDataSource(file);
-//            this.player.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
+            this.player.setAudioStreamType(AudioManager.STREAM_ALARM);
             //if it's a streaming file, play mode is implied
             this.setMode(MODE.PLAY);
             this.setState(STATE.MEDIA_STARTING);
