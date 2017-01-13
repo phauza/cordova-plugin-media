eee028d738657acf65b8a14374c24269f0605f60
Piotr.Hauza <p.hauza@cctechnology.pl>
2017-01-13 14:34:58 +0100

    ph01

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -299,6 +299,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
         if (this.readyPlayer(file) && this.player != null) {
 //            this.player.setAudioStreamType(streamType);
             this.player.setAudioStreamType(AudioManager.STREAM_ALARM);
+            this.player.prepare();
             this.player.start();
             this.setState(STATE.MEDIA_RUNNING);
             this.seekOnPrepared = 0; //insures this is always reset
