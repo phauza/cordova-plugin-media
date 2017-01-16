da179013ace9e2a8626424f21ce344e0825155e0
Piotr.Hauza <p.hauza@cctechnology.pl>
2017-01-16 11:22:45 +0100

    ph01

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -302,8 +302,6 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
     Log.d(LOG_TAG, "---------------------------");
     Log.d(LOG_TAG, "---------------------------startPlaying file:" + file);
 
-    player.start()
-
         if (this.readyPlayer(file) && this.player != null) {
 //            this.player.setAudioStreamType(streamType);
 
